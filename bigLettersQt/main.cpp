a502a034dc4b1040f0dd3447d37dc32618d856de
Diana Ivanchevska <ivanchevska.diana@gmail.com>
2020-10-21 17:50:10 +0300

    +optimization

--- bigLettersQt/main.cpp
+++ bigLettersQt/main.cpp
@@ -3,9 +3,15 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-using namespace std;
 
-int main(int argc, char *argv[])
+using std::cin;//instead of using namespace std; better to list only names that will be used
+using std::cout;
+using std::vector;
+using std::string;
+using std::endl;
+
+
+int main(int argc, char* argv[])
 {
     QCoreApplication a(argc, argv);
 
@@ -27,28 +33,28 @@ int main(int argc, char *argv[])
     {
         string str;
         getline(cin, str); //getline() is used to read chars till the newline character '\n'
-        letters[i] = str;
-     }
+        letters.at(i) = str;
+    }
 
-        cout << endl << endl;
+    cout << endl << endl;
 
     for (size_t i = 0; i < letters.size(); i++) // for loop with one nested loop is used to iterate through entered text and to construct result string
     {
         string resultStr = "";
         for (size_t j = 0; j < text.size(); j++)
         {
-            auto intLetter = static_cast<int>(text[j]);
+            int intLetter = static_cast<int>(text[j]);
             if (intLetter < A || intLetter > Z)
             {
-               resultStr += letters[i].substr(width * (Z - A + 1), width); //if entered other than A-Z letters, will output ? sign according to the entered template
+                resultStr += letters.at(i).substr(width * (Z - A + 1), width); //if entered other than A-Z letters, will output ? sign according to the entered template
             }
             else
             {
-               resultStr += letters[i].substr(width * (intLetter - A), width); //for output of A-Z letters according to the entered template
+                resultStr += letters.at(i).substr(width * (intLetter - A), width); //for output of A-Z letters according to the entered template
             }
-         }
+        }
 
-         cout << resultStr << endl;
-     }
+        cout << resultStr << endl;
+    }
     return 0;
 }
